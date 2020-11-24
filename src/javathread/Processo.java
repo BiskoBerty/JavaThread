42bf72eb8d55ed555c9d40be362c6eae11ffe80d
galimberti.riccardoe <galimberti.riccardoe@TAVE-PC14.OSDBSESTO.local>
2020-11-24 13:38:14 +0100

    init repo

---
+++ src/javathread/Processo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javathread;
+
+/**
+ *
+ * @author galimberti.riccardoe
+ */
+public class Processo extends Thread{
+    private String s;
+
+    public Processo(String s) {
+        this.s = s;
+    }
+    public void run(){
+        for(;;) System.out.println(s);
+    }
+    
+}
+    
